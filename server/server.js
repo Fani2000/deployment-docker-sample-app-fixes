cfb0d3b232c95fc97ef2fda65c7b52bfaafc9814
Keorapetse Fani <fkeorapetse@quickloc8.co.za>
2025-06-18 22:48:50 +0200

    Updated the services for the backend and also docker compose

--- server/server.js
+++ server/server.js
@@ -1,284 +1,233 @@
 import express from 'express';
-import bodyParser from 'body-parser'
+import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import slackChannelModel from './roomModel.js'; //dbmodel
-import userModel from './userModel.js'
-import slackMessageModel from './messageModel.js'
-// app config 
+import slackChannelModel from './roomModel.js';
+import userModel from './userModel.js';
+import slackMessageModel from './messageModel.js';
 import cors from 'cors';
-const app=express();
+
+const app = express();
 const port = 9000;
 
-//middle ware needed to parse json body 
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.json())
-app.use(cors())
-// app.use((req,res,next) => {
-//     // setting the response headers for open cors access 
-//     // res.setHeader("Access-Control-Allow-Origin", "*");
-//     // res.setHeader("Access-Control-Allow-Headers", "*");
-//     // next();
+// DB Config
+const dbName = process.env.DB_NAME ?? 'slackchannels';
+const hostName = process.env.DB_HOSTNAME ?? 'db';
 
-// })
-// https://www.udemy.com/course/the-complete-web-development-bootcamp/learn/lecture/13559534#overview
-// db config 
-const connection = "mongodb://db:27017/slackchannels";
-// const connection = "mongodb://localhost:27017/slackchannels";
+const connection = `mongodb://${hostName}:27017/${dbName}`;
 
 mongoose.connect(connection, {
- 
     useNewUrlParser: true,
-    // useCreateIndex: true,
     useUnifiedTopology: true
- 
 }).then(() => {
     console.log('successfully connected to the database');
-
-
-
 }).catch(err => {
     console.log('error connecting to the database');
-    console.log(err)
+    console.log(err);
     process.exit();
 });
 
-// run initially to populate database which is mounted locally 
-// record = json object 
+// Routes
+app.get('/', (req, res) => {
+    console.log('connecting to route route "/"');
+    res.status(200).send('Slack Clone');
+});
 
+app.post('/login', async (req, res) => {
+    try {
+        const requestData = req.body;
+        // Search for existing user
+        const existingUser = await userModel.findOne(requestData);
 
-app.get('/', (req,res) => {
-    console.log('connecting to route route "/"')
-    res.status(200).send('Slack Clone')
-})
-
-
-
-app.post('/login', (req,res) => {
-    
-
-    const requestData = req.body
-    
-    // searches for user if found return user data object, if not found then add user to database
-    userModel.findOne(requestData, (err, data) => {
-        if (!err) {             
-
-            if (data !== null ) {
-                console.log('Found user!!!!!!')
-                console.log(data)
-                res.status(201).send(data)
-            }
-            
-            
-            else if (data === null ) {
-                console.log('data was null user does not exist in db: creating')
-                
-                const user = { 
-                    email:  req.body.email,
-                    password: req.body.password,
-                    userImage: req.body.userImage
-
-
-                }
-                console.log(user)
-
-                const newUser = new userModel(user)
-            
-                newUser.save((err) => {
-                    if (!err) {
-                        console.log(`User ${user} has been registered`)                    
-
-                        res.status(202).send(user)
-                       
-                        
-            
-                    } else {
-                        // there was an error 
-                        res.status(400).send(err)
-            
-                    }
-                })
-            }          
-           
-
-        } 
-    })
-
-
-    // res.status(200).send('Slack Clone')
-})
-
-
-app.get('/v1/channels/', (req,res) => {
-
-    console.log('connecting to route route "/channels"')
-    slackChannelModel.find((err,data) => {
-        if (!err) {
-            console.log(typeof(data))
-            
-            console.log(data)    
-                            
-            // 201 -> created 
-            res.status(201).send(data)
-            
-
-        } else {
-            console.log(err)
-            res.status(500).send(err)
+        if (existingUser) {
+            console.log('Found user!!!!!!');
+            console.log(existingUser);
+            return res.status(201).send(existingUser);
         }
 
-        
-    } )
-})
+        // If user doesn't exist, create new user
+        console.log('data was null user does not exist in db: creating');
+        const user = {
+            email: req.body.email,
+            password: req.body.password,
+            userImage: req.body.userImage
+        };
+        console.log(user);
 
-app.get('/v1/channels/find', (req,res) => {
+        const newUser = new userModel(user);
+        await newUser.save();
 
+        console.log(`User ${JSON.stringify(user)} has been registered`);
+        return res.status(202).send(user);
 
-    slackChannelModel.find((err,data) => {
-        if (!err) {
-            console.log(typeof(data))
-            
-            console.log(data)    
-                            
-            // 201 -> created 
-            res.status(201).send(data)
-            
-
-        } else {
-            console.log(err)
-            res.status(500).send(err)
-        }
-
-        
-    } )
-})
-
-
-
-
-// add channel 
-app.post('/v1/channels/add', (req,res) => {
-    const newChannel = req.body
-
-    slackChannelModel.create(newChannel, (err, data) => {
-        if (!err) {          
-            console.log('Entry successful')
-            console.log(newChannel)   
-            console.log(data)        
-            res.status(201).send(data)
-
-        } else {
-            console.log('Entry unsuccessful')
-            console.log(err)
-        }
-    })
-})
-
-
-app.post('/v1/channels/getChannelDetail', (req,res) => {
-    const requestData = req.body
-
-    slackChannelModel.findOne(requestData, (err, data) => {
-        if (!err) {          
-            console.log(`${data} retreived from the database`)                 
-            res.status(201).send(data)
-
-        } else {
-            console.log('There was an error locating data')
-            console.log(err)
-        }
-    })
-})
-
-
-app.post('/newMessage', (req,res) => {
-   
-
-    const messageContent = {
-        message: req.body.message,
-        timeStamp: req.body.timeStamp,
-        user: req.body.user,
-        userImage: req.body.userImage,
-        channelId: req.body.channelId
+    } catch (err) {
+        console.error('Error:', err);
+        return res.status(400).send(err);
     }
+});
 
-        const newMessage = new slackMessageModel(messageContent)
-
-
-        newMessage.save((err) => {
-            if (!err) {
-                console.log(`${messageContent} has been saved successfully`)                    
-
-                res.status(202).send(messageContent)
-               
-                
-    
-            } else {
-                // there was an error 
-                res.status(400).send(err)
-    
-            }
-        })
-        
-
-
-}) 
-
-
-
-app.post('/v1/channels/findMessage', (req,res) => {
-    const channel = req.body
-
-
-    slackMessageModel.find((err,data) => {
-        if (!err) {
-            console.log(typeof(data))
-            
-            console.log(data)    
-                            
-            // 201 -> created 
-            res.status(201).send(data)
-            
-
-        } else {
-            console.log(err)
-            res.status(500).send(err)
-        }
-
-        
-    } )
-})
-
-
-app.get('/v1/channels/findMessage/:roomID', (req,res) => {  
-
-
-    const roomID = req.params.roomID
-
-    const query = {
-        channelId: roomID
+app.get('/v1/channels/', async (req, res) => {
+    try {
+        console.log('connecting to route route "/channels"');
+        const data = await slackChannelModel.find();
+        console.log(typeof(data));
+        console.log(data);
+        return res.status(201).send(data);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send(err);
     }
+});
 
-    slackMessageModel.find(query,(err,data) => {
-        if (!err) {
-            // console.log(typeof(data))
-            
-            // console.log(data)    
-                            
-            // 201 -> created 
-            res.status(201).send(data)
-            
+app.get('/v1/channels/find', async (req, res) => {
+    try {
+        const data = await slackChannelModel.find();
+        console.log(typeof(data));
+        console.log(data);
+        return res.status(201).send(data);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send(err);
+    }
+});
 
+app.post('/v1/channels/add', async (req, res) => {
+    try {
+        const newChannel = req.body;
+        const data = await slackChannelModel.create(newChannel);
+        return res.status(201).send(data);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send(err);
+    }
+});
+app.post('/v1/channels/getChannelDetail', async (req, res) => {
+    try {
+        const requestData = req.body;
+        const data = await slackChannelModel.findOne(requestData);
+        if (data) {
+            console.log(`${data} retrieved from the database`);
+            return res.status(201).send(data);
         } else {
-            console.log(err)
-            res.status(500).send(err)
+            console.log('No data found');
+            return res.status(404).send({ message: 'Channel not found' });
         }
+    } catch (err) {
+        console.log('There was an error locating data');
+        console.error(err);
+        return res.status(500).send(err);
+    }
+});
 
+app.post('/newMessage', async (req, res) => {
+    try {
+        const messageContent = {
+            message: req.body.message,
+            timeStamp: req.body.timeStamp,
+            user: req.body.user,
+            userImage: req.body.userImage,
+            channelId: req.body.channelId
+        };
+
+        const newMessage = new slackMessageModel(messageContent);
+        await newMessage.save();
         
-    } )
-})
+        console.log(`${JSON.stringify(messageContent)} has been saved successfully`);
+        return res.status(202).send(messageContent);
+    } catch (err) {
+        console.error('Error saving message:', err);
+        return res.status(400).send(err);
+    }
+});
 
-app.listen(port, () => console.log(`Listening on port : ${port}`))
+app.post('/v1/channels/findMessage', async (req, res) => {
+    try {
+        const data = await slackMessageModel.find();
+        console.log(typeof(data));
+        console.log(data);
+        return res.status(201).send(data);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send(err);
+    }
+});
 
+app.get('/v1/channels/findMessage/:roomID', async (req, res) => {
+    try {
+        const roomID = req.params.roomID;
+        const query = {
+            channelId: roomID
+        };
+        
+        const data = await slackMessageModel.find(query);
+        return res.status(201).send(data);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send(err);
+    }
+});
 
-// docker run -p 27017:27017 -v /Users/ellarnol/localSlackDatabase:/data/db  mongo:latest
+// Messages routes
+app.get('/v1/messages/sync', async (req, res) => {
+    try {
+        const data = await slackMessageModel.find();
+        return res.status(200).send(data);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send(err);
+    }
+});
+
+app.post('/v1/messages/new', async (req, res) => {
+    try {
+        const dbMessage = req.body;
+        const data = await slackMessageModel.create(dbMessage);
+        return res.status(201).send(data);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send(err);
+    }
+});
+
+app.get('/v1/messages/:channelId', async (req, res) => {
+    try {
+        const channelId = req.params.channelId;
+        const data = await slackMessageModel.find({ channelId: channelId });
+        return res.status(200).send(data);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send(err);
+    }
+});
+
+// Delete routes
+app.delete('/v1/channels/:channelId', async (req, res) => {
+    try {
+        const channelId = req.params.channelId;
+        await slackChannelModel.deleteOne({ _id: channelId });
+        return res.status(200).send({ message: 'Channel deleted successfully' });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send(err);
+    }
+});
+
+app.delete('/v1/messages/:messageId', async (req, res) => {
+    try {
+        const messageId = req.params.messageId;
+        await slackMessageModel.deleteOne({ _id: messageId });
+        return res.status(200).send({ message: 'Message deleted successfully' });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send(err);
+    }
+});
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
